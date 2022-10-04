577310400c9259004afe010eb068febe416ec204
Alan McKay <amckay2@uiowa.edu>
2022-10-04 18:02:17 -0500

    Added options for cli parameters for running the client. User running file can now supply a message, port number, and host address through the cli

--- sandbox/echo_client.py
+++ sandbox/echo_client.py
@@ -1,7 +1,17 @@
 import socket
+import sys
 
-HOST = "127.0.0.1"
+#!! Defaults:
+MESSAGE = "Hello World"
 PORT = 65432
+HOST = "127.0.0.1"
+
+if len(sys.argv) >= 2:
+    MESSAGE = sys.argv[1]
+if len(sys.argv) >= 3:
+    PORT = str(sys.argv[2])
+if len(sys.argv) >= 4:
+    HOST = sys.argv[3]
 
 def echo_client(host,port,message):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,4 +29,4 @@ def echo_client(host,port,message):
         s = None
     return data
 
-print(echo_client(HOST,PORT,"Hello World; test!"))
+print(echo_client(HOST,PORT,MESSAGE))
