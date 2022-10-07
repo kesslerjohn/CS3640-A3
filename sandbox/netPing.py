6c92c61f2c1884af5b536d1d959496508a89824e
kesslerjohn <john-kessler@uiowa.edu>
2022-10-07 17:41:58 -0500

    Update netPing.py
    A file containing some actual characters

---
+++ sandbox/netPing.py
@@ -0,0 +1,21 @@
+import socket
+import dpkt
+
+'''
+pseudocode for sending icmp echo
+
+def send_icmp_echo(socket, payload, id, seq, destination):
+    echo = dpkt.icmp.ICMP.Echo()
+    echo.id = id
+    echo.seq = seq
+    echo.data = payload
+    socket.send
+
+'''
+
+def send_icmp_echo(socket, payload, id, seq, destination):
+    echo = dpkt.icmp.ICMP.Echo()
+    echo.id = id
+    echo.seq = seq
+    echo.data = payload
+    socket.send()
