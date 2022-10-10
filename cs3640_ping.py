b0d5245f18be40a49e094a9a65fe88b6210ab381
Nalini Singh <112131354+nalinisingh03@users.noreply.github.com>
2022-10-10 16:57:44 -0500

    update ttl in make

--- cs3640_ping.py
+++ cs3640_ping.py
@@ -1,20 +1,13 @@
 import socket
 import dpkt
 
-'''
-Some additional code pieces:
-for setting socket ttl: 
-    sock.IP_TTL = ttl
-
-for creating a socket:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, dpkt.ip.IP_PROTO_ICMP)
-'''
 
 def make_icmp_socket(ttl, timeout):
     s1 = socket.AF_INET
     s2 = socket.SOCK_RAW
     s3 = dpkt.ip.IP_PROMO_ICMP
     sock = socket.socket(s1, s2, s3)
+    sock.setsockopt(socket.IP_TTL, ttl)
 
 def send_icmp_echo(sock, payload, id, seq, destination):
     echo = dpkt.icmp.ICMP.Echo()
