87c14b13c4ea282ec92f7933cac83ddd57e96a43
kesslerjohn <john-kessler@uiowa.edu>
2022-10-10 16:59:42 -0500

    Update cs3640_ping.py

--- cs3640_ping.py
+++ cs3640_ping.py
@@ -1,5 +1,6 @@
 import socket
 import dpkt
+import sys
 
 '''
 Some additional code pieces:
@@ -15,6 +16,7 @@ def make_icmp_socket(ttl, timeout):
     s2 = socket.SOCK_RAW
     s3 = dpkt.ip.IP_PROMO_ICMP
     sock = socket.socket(s1, s2, s3)
+    return sock
 
 def send_icmp_echo(sock, payload, id, seq, destination):
     echo = dpkt.icmp.ICMP.Echo()
@@ -29,3 +31,19 @@ def send_icmp_echo(sock, payload, id, seq, destination):
     #sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, dpkt.ip.IP_PROTO_ICMP)
     sock.connect(destination, 1)
     sent = sock.send(str(icmp))
+
+def main():
+    n = len(sys.argv)
+    if (n <= 1):
+        print("")
+    destination = n[2]
+    payload = n[3]
+    ttl = n[4] #todo check the indices of these command line args - John
+    
+
+    id = 0x81
+    seq = 0x7E
+    timeout = ttl*1000 #Is this right? - John
+    skt = make_icmp_socket(ttl, timeout)
+    send_icmp_echo(skt, payload, id, seq, destination)
+
