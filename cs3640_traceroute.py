691913b880d9169ef86547b9fba50841edd92575
mscudiero <114504592+mscudiero@users.noreply.github.com>
2022-10-13 09:53:29 -0500

    Update cs3640_traceroute.py
    Added logic for traceroute.py

--- cs3640_traceroute.py
+++ cs3640_traceroute.py
@@ -16,6 +16,23 @@ def main():
             print("Error: some parameters missing.") #todo add a while loop to ping forever if
                                                      #no value is given for n_hops. - John
             return 1
+    ttl = 1
+
+    while (ttl <= num):
+        sock = cs3640_ping.make_icmp_socket(ttl, ttl*1000)
+        startTime = time.time()
+        cs3640_ping.send_icmp_echo(sock, "Hello World", 0x81, 0x7E, dst)
+        recvr = cs3640_ping.recv_icmp_response()
+        endTime = time.time()
+        RTT = (endTime - startTime) * 1000
+        round(RTT, 4)
+        addr = recvr[1]
+        print("destination = " + dst + "; hop " + ttl + " = " + addr + "; rtt = " + RTT + " ms")
+        if (addr == dst):
+            break
+        else:
+            ttl = ttl + 1
+
     return 0
 if __name__ == "__main__":
     main()
