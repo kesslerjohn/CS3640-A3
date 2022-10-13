6db8a205fd8795b552b66032c5caae0605f3acee
mscudiero <114504592+mscudiero@users.noreply.github.com>
2022-10-13 10:05:36 -0500

    Update cs3640_traceroute.py
    Casting n_hops as an int for comparison.

--- cs3640_traceroute.py
+++ cs3640_traceroute.py
@@ -19,7 +19,7 @@ def main():
             return 1
     ttl = 1
 
-    while (ttl <= num):
+    while (ttl <= int(num)):
         sock = cs3640_ping.make_icmp_socket(ttl, ttl*1000)
         startTime = time.time()
         cs3640_ping.send_icmp_echo(sock, "Hello World", 0x81, 0x7E, dst)
